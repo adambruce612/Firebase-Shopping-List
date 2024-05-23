8a6a26efec0449208f88b88ba390804d58ef4e96
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-05-22 21:03:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -23,18 +23,24 @@ addButtonEl.addEventListener("click", function() {
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.entries(snapshot.val());
-
-    clearShoppingListEl();
-    
-    for (let i = 0; i < itemsArray.length; i++)
+    if(snapshot.exists())
         {
-            let currentItem = itemsArray[i];
-            let currentItemID = currentItem[0];
-            let currentItemValue = currentItem[1];
+            let itemsArray = Object.entries(snapshot.val());
+
+            clearShoppingListEl();
+    
+            for (let i = 0; i < itemsArray.length; i++)
+                {
+                    let currentItem = itemsArray[i];
+                    let currentItemID = currentItem[0];
+                    let currentItemValue = currentItem[1];
              
-            appendItemToShoppingListEl(currentItem);
+                    appendItemToShoppingListEl(currentItem);
+                }
+        } else {
+            shoppingListEl.innerHTML = "No items here...yet";
         }
+
 });
 
 function clearShoppingListEl() {
